945d263cbe39622f205beece9bc35c0efa1adcfb
laibin5 <laibin5@gdii-yueyun.com>
2021-11-10 09:19:46 +0800

    add webpack for test

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,20 @@ module.exports = {
   entry: __dirname + "/app/index.js",
   output: {
     path: __dirname + "/public",//打包后的文件存放的地方
-    filename: 'bundle.js',
-    // filename: "[name].js", //打包后输出文件的文件名
-    // chunkFilename: '[name].js',
+    //use weak webpack
+    // filename: 'bundle.js',
+    //use webpack
+    path: __dirname + "/dist",//打包后的文件存放的地方
+    filename: "[name].js", //打包后输出文件的文件名
+    chunkFilename: '[name].js',
+    //use webpack
   },
   mode: 'development',
   // devtool: false,
 
-  // optimization: {
-  //   runtimeChunk: true,
-  //   splitChunks: {
-  //     chunks: "initial", 
-  //     automaticNameDelimiter: '~',  
-  //     name: true,
-  //     cacheGroups: { // 缓存组
-  //       vendors: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //       },
-  //     }
-  //   }
-  // },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  }
 }
